04e16c13bfe4d449bc2b74d40d41e70401a65908
pofce <vladyslav.radchenko.ki.2019@lpnu.ua>
2024-12-03 22:28:42 +0200

    GAN added

--- models/gan.py
+++ models/gan.py
@@ -1,47 +1,47 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
-from torch.nn.functional import adaptive_avg_pool2d, interpolate
-import torchvision.models as models
 from scipy import linalg
 
 
+# Generator and Discriminator classes remain the same
 class Generator(nn.Module):
     def __init__(self, ngpu, nc, nz, ngf):
         super(Generator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
+            # Input is Z, going into a convolution
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
+            # State size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
+            # State size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
+            # State size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
+            # State size. (ngf) x 32 x 32
             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
+            # Output size. (nc) x 64 x 64
         )
 
     def forward(self, input):
         return self.main(input)
 
 
-# custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if 'Conv' in classname:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
+    elif 'BatchNorm' in classname:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
@@ -51,187 +51,79 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
+            # Input is (nc) x 64 x 64
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
+            # State size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+            # State size. (ndf*2) x 16 x 16
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
+            # State size. (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
+            # State size. (ndf*8) x 4 x 4
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
+            # Output size. 1
         )
 
     def forward(self, input):
         return self.main(input)
 
 
-class InceptionV3(nn.Module):
-    """Pretrained InceptionV3 network returning feature maps"""
-
-    # Index of default block of inception to return,
-    # corresponds to output of final average pooling
-    DEFAULT_BLOCK_INDEX = 3
-
-    # Maps feature dimensionality to their output blocks indices
-    BLOCK_INDEX_BY_DIM = {
-        64: 0,  # First max pooling features
-        192: 1,  # Second max pooling featurs
-        768: 2,  # Pre-aux classifier features
-        2048: 3  # Final average pooling features
-    }
-
-    def __init__(self,
-                 output_blocks=[DEFAULT_BLOCK_INDEX],
-                 resize_input=True,
-                 normalize_input=True,
-                 requires_grad=False):
-
-        super(InceptionV3, self).__init__()
-
-        self.resize_input = resize_input
-        self.normalize_input = normalize_input
-        self.output_blocks = sorted(output_blocks)
-        self.last_needed_block = max(output_blocks)
-
-        assert self.last_needed_block <= 3, \
-            'Last possible output block index is 3'
-
-        self.blocks = nn.ModuleList()
-
-        inception = models.inception_v3(pretrained=True)
-
-        # Block 0: input to maxpool1
-        block0 = [
-            inception.Conv2d_1a_3x3,
-            inception.Conv2d_2a_3x3,
-            inception.Conv2d_2b_3x3,
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        ]
-        self.blocks.append(nn.Sequential(*block0))
-
-        # Block 1: maxpool1 to maxpool2
-        if self.last_needed_block >= 1:
-            block1 = [
-                inception.Conv2d_3b_1x1,
-                inception.Conv2d_4a_3x3,
-                nn.MaxPool2d(kernel_size=3, stride=2)
-            ]
-            self.blocks.append(nn.Sequential(*block1))
-
-        # Block 2: maxpool2 to aux classifier
-        if self.last_needed_block >= 2:
-            block2 = [
-                inception.Mixed_5b,
-                inception.Mixed_5c,
-                inception.Mixed_5d,
-                inception.Mixed_6a,
-                inception.Mixed_6b,
-                inception.Mixed_6c,
-                inception.Mixed_6d,
-                inception.Mixed_6e,
-            ]
-            self.blocks.append(nn.Sequential(*block2))
-
-        # Block 3: aux classifier to final avgpool
-        if self.last_needed_block >= 3:
-            block3 = [
-                inception.Mixed_7a,
-                inception.Mixed_7b,
-                inception.Mixed_7c,
-                nn.AdaptiveAvgPool2d(output_size=(1, 1))
-            ]
-            self.blocks.append(nn.Sequential(*block3))
-
-        for param in self.parameters():
-            param.requires_grad = requires_grad
-
-    def forward(self, inp):
-        outp = []
-        x = inp
-
-        if self.resize_input:
-            x = interpolate(x, size=(299, 299), mode='bilinear', align_corners=False)
-
-        if self.normalize_input:
-            x = 2 * x - 1  # Scale from range (0, 1) to range (-1, 1)
-
-        for idx, block in enumerate(self.blocks):
-            x = block(x)
-            if idx in self.output_blocks:
-                outp.append(x)
-
-            if idx == self.last_needed_block:
-                break
-
-        return outp
-
-
-def calculate_activation_statistics(images, model, batch_size=128, dims=2048,
-                                    cuda=False):
+# Corrected FID calculation functions
+def calculate_activation_statistics(images, model, cuda=False):
     model.eval()
-
-    if cuda:
-        batch = images.cuda()
-    else:
-        batch = images
-    pred = model(batch)[0]
-
-    if pred.size(2) != 1 or pred.size(3) != 1:
-        pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
-
-    act = pred.cpu().data.numpy().reshape(pred.size(0), -1)
-
+    with torch.no_grad():
+        if cuda:
+            images = images.cuda()
+        else:
+            images = images.cpu()
+        # Resize images to (299, 299)
+        images_resized = F.interpolate(images, size=(299, 299), mode='bilinear', align_corners=False)
+        # Normalize images to [-1, 1]
+        images_normalized = 2 * images_resized - 1
+        pred = model(images_normalized)['pool']  # 'pool' is the layer name for the final average pooling
+        act = pred.cpu().numpy().reshape(pred.size(0), -1)
     mu = np.mean(act, axis=0)
     sigma = np.cov(act, rowvar=False)
     return mu, sigma
 
 
 def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
-
+    """Compute the Frechet Distance between two multivariate Gaussians."""
     mu1 = np.atleast_1d(mu1)
     mu2 = np.atleast_1d(mu2)
 
     sigma1 = np.atleast_2d(sigma1)
     sigma2 = np.atleast_2d(sigma2)
 
-    assert mu1.shape == mu2.shape, 'Training and test mean vectors have different lengths'
-    assert sigma1.shape == sigma2.shape, 'Training and test covariances have different dimensions'
-
     diff = mu1 - mu2
 
+    # Compute square root of product of covariance matrices
     covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    # Numerical stability
     if not np.isfinite(covmean).all():
-        msg = ('fid calculation produces singular product; '
-               'adding %s to diagonal of cov estimates') % eps
-        print(msg)
         offset = np.eye(sigma1.shape[0]) * eps
         covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
 
+    # Avoid imaginary numbers
     if np.iscomplexobj(covmean):
-        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
-            m = np.max(np.abs(covmean.imag))
-            raise ValueError('Imaginary component {}'.format(m))
         covmean = covmean.real
 
     tr_covmean = np.trace(covmean)
 
-    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
+    fid = (diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean)
+    return fid
 
 
-def calculate_fretchet(images_real, images_fake, model):
-    mu_1, std_1 = calculate_activation_statistics(images_real, model, cuda=True)
-    mu_2, std_2 = calculate_activation_statistics(images_fake, model, cuda=True)
-
-    """get fretched distance"""
-    fid_value = calculate_frechet_distance(mu_1, std_1, mu_2, std_2)
+def calculate_fretchet(real_images, fake_images, model, cuda=False):
+    mu1, sigma1 = calculate_activation_statistics(real_images, model, cuda=cuda)
+    mu2, sigma2 = calculate_activation_statistics(fake_images, model, cuda=cuda)
+    fid_value = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
     return fid_value
