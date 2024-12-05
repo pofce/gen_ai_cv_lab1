54231885e8d537ea6e2a426a875c555a40dd9c67
pofce <vladyslav.radchenko.ki.2019@lpnu.ua>
2024-12-05 11:44:43 +0200

    Autoencoder done

--- models/ae.py
+++ models/ae.py
@@ -6,11 +6,11 @@ class Autoencoder(nn.Module):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 12, 4, stride=2, padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(12, 24, 4, stride=2, padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(24, 48, 4, stride=2, padding=1),
-            nn.ReLU(),
+            nn.LeakyReLU(0.2),
         )
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(48, 24, 4, stride=2, padding=1),
